064d0a0edf969518ea665d3b28e2133bce8541c9
ujjani018 <ujjani018@gmail.com>
2026-03-12 09:33:05 +0530

    Create  MatrixSubtraction.java

---
+++ MatrixSubtraction.java
@@ -0,0 +1,51 @@
+ import java.util.Scanner;
+
+public class MatrixSubtraction {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int rows, cols;
+
+        System.out.print("Enter number of rows: ");
+        rows = sc.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        cols = sc.nextInt();
+
+        int[][] matrix1 = new int[rows][cols];
+        int[][] matrix2 = new int[rows][cols];
+        int[][] sum = new int[rows][cols];
+
+        System.out.println("Enter elements of first matrix:");
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                matrix1[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Enter elements of second matrix:");
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                matrix2[i][j] = sc.nextInt();
+            }
+        }
+
+        
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                sum[i][j] = matrix1[i][j] - matrix2[i][j];
+            }
+        }
+
+        System.out.println("subtraction of matrices:");
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                System.out.print(sum[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
+}
